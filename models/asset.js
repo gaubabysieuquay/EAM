9090870e762894b44fe53672f0a0b661dd5e2377
gaubabysieuquay <51057860+gaubabysieuquay@users.noreply.github.com>
2020-12-11 15:34:15 +0700

    Modify

--- models/asset.js
+++ models/asset.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Asset extends Model {
     /**
@@ -12,23 +10,42 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Asset.init({
-    barcode: DataTypes.STRING,
-    name: DataTypes.STRING,
-    serial: DataTypes.STRING,
-    model: DataTypes.STRING,
-    purchaseDate: DataTypes.DATE,
-    purchaseCost: DataTypes.INTEGER,
-    status: DataTypes.INTEGER,
-    warranty: DataTypes.INTEGER,
-    note: DataTypes.STRING,
-    image: DataTypes.STRING,
-    supplierId: DataTypes.INTEGER,
-    locationId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Asset',
-  });
+  }
+  Asset.init(
+    {
+      barcode: DataTypes.STRING,
+      name: DataTypes.STRING,
+      serial: DataTypes.STRING,
+      model: DataTypes.STRING,
+      purchaseDate: DataTypes.DATE,
+      purchaseCost: DataTypes.INTEGER,
+      status: DataTypes.INTEGER,
+      warranty: DataTypes.INTEGER,
+      note: DataTypes.STRING,
+      image: DataTypes.STRING,
+      supplierId: {
+        type: DataTypes.INTEGER,
+        onDelete: "cascade",
+        onUpdate: "cascade",
+        references: {
+          model: "Supplier",
+          key: "id",
+        },
+      },
+      locationId: {
+        type: DataTypes.INTEGER,
+        onDelete: "cascade",
+        onUpdate: "cascade",
+        references: {
+          model: "Location",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Asset",
+    }
+  );
   return Asset;
 };
