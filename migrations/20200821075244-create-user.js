f838fd9218ba9c31e98ccc7b63b3ea0f57ece02d
gaubabysieuquay <51057860+gaubabysieuquay@users.noreply.github.com>
2020-12-14 20:19:18 +0700

    Modify

--- migrations/20200821075244-create-user.js
+++ migrations/20200821075244-create-user.js
@@ -17,6 +17,30 @@ module.exports = {
       password: {
         type: Sequelize.STRING,
       },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      address: {
+        type: Sequelize.STRING,
+      },
+      city: {
+        type: Sequelize.STRING,
+      },
+      state: {
+        type: Sequelize.STRING,
+      },
+      country: {
+        type: Sequelize.STRING,
+      },
+      phone: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
