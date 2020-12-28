7a70875e3b7d8f21688b4d38295d091b6d6e795d
gaubabysieuquay <51057860+gaubabysieuquay@users.noreply.github.com>
2020-12-29 00:54:35 +0700

    Rename

--- migrations/20200827082908-create-user-role.js
+++ migrations/20200827082908-create-user-role.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("UserRoles", {
+    await queryInterface.createTable("User_roles", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -39,6 +39,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("UserRoles");
+    await queryInterface.dropTable("User_roles");
   },
 };
